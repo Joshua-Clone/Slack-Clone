157301314f43ab93199168b4838ffd4a83a1da76
jsingle345 <jsingle345@gmail.com>
2021-03-29 20:13:32 -0500

    Began on the header component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <>
+        <Switch>
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+        </>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+// Install React Router 
+// In Terminal, type: "npm i react-router-dom"
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCCjhlx5CuxfZ4ZjIRpUG8oYni9N7VYRNQ",
+    authDomain: "slack-clone-689ef.firebaseapp.com",
+    projectId: "slack-clone-689ef",
+    storageBucket: "slack-clone-689ef.appspot.com",
+    messagingSenderId: "1028935916312",
+    appId: "1:1028935916312:web:15c7ce382f91ea9aa6dc6d",
+    measurementId: "G-6GY50T5Y4V"
+  };
+
+  // Taken from firebase Project Settings under Firebase SDK Config
