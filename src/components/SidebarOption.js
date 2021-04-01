80e60afe0f5d7340d2931460711ef4c7a739dd44
jsingle345 <jsingle345@gmail.com>
2021-04-01 15:45:18 -0500

    Connected the redux store to the sidebar

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { useDispatch } from "react-redux"; 
 import styled from "styled-components"
+import { enterRoom } from "../features/appSlice";
 import { db } from "../firebase";
 
 function SidebarOption({ Icon, title, addChannelOption, id }) {
+    const dispatch = useDispatch(); 
 
     const addChannel = () => {
         // in terminal type "npm add firebase"
@@ -18,7 +21,13 @@ function SidebarOption({ Icon, title, addChannelOption, id }) {
     }; 
 
     const selectChannel = () => {
-        
+        if (id) {
+            dispatch(
+                enterRoom({
+                    roomId: id
+                })
+            )
+        }
     }
 
     return (
